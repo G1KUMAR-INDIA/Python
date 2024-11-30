d2f993bca583ad153644823b274bb9d74d399fff
R JEEVAN KUMAR <g1kumar215@gmail.com>
2024-11-30 09:41:19 +0530

    Day 1 Python Class

---
+++ 1st class/test.py
@@ -0,0 +1,17 @@
+print("Hello World")
+print(2+3)
+print(3-2)
+print(2*3)
+print(3/2)
+print(3//2)
+print(3%2)
+print(3**2)
+print(2**3)
+a=1
+print(a)
+a=1.1
+print(a)
+a="G1KUMAR"
+print(a)
+a,b,c,d,e=1,2,3,4,5
+print(a+b+c+d+e)
