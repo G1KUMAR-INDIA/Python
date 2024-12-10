c5880fc70c3bfdf244dcca3b31ae6e91bff7c7e2
R JEEVAN KUMAR <g1kumar215@gmail.com>
2024-12-10 18:51:52 +0530

    Session 4

---
+++ 4th session/Test.java
@@ -0,0 +1,30 @@
+// public class test
+// {
+//     public static void main(String[] args)
+//     {
+//         int a,b;
+//         a=10;
+//         b=20;
+//         System.out.println(a+b);
+//     }
+// }
+// public class test
+// {
+//     public static void main(String[] args)
+//     {
+//         String a,b;
+//         a="10";
+//         b="20";
+//         System.out.println(a+b);
+//     }
+// }
+public class test
+{
+    public static void main(String[] args)
+    {
+        var a=10;
+        var b=20;
+        // var c=true
+        System.out.println(a+b);
+    }
+}
---
+++ 4th session/test.py
@@ -0,0 +1,6 @@
+a=10
+print(type(a))
+a=True
+print(type(a))
+a="G1KUMAR"
+print(type(a))
